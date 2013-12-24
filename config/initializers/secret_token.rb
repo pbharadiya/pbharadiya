d24c1f6b1ed27fb766a9b7152f33ecbb01bf4c49
pbharadiya <parthbharadiya007@gmail.com>
2013-12-24 12:30:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbharadiya::Application.config.secret_token = 'c462becf8c39b5667d1c8613691d7f6af5f4505bb48027d3c778428a59442d7250582d2eada3cf710d448fc4bb811164832ec7f115c0839d383bec371bd74a88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pbharadiya::Application.config.session_store :cookie_store, key: '_pbharadiya_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pbharadiya::Application.config.session_store :active_record_store
